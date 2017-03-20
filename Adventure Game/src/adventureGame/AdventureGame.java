c6cb0c8b4016e022ebb2e4728da2ec976ed8590d
323966051 <323966051@C-DS24WR1.tdsb.on.ca>
2017-03-20 15:15:17 -0400

    adventure game push take 2

--- Adventure Game/src/adventureGame/AdventureGame.java
+++ Adventure Game/src/adventureGame/AdventureGame.java
@@ -35,17 +35,10 @@ public class AdventureGame {
 		BufferedImage lasersecurity = null;
 		BufferedImage museum1 = null;
 		BufferedImage museum2 = null;
-		BufferedImage museum3 = null;
-		BufferedImage museum4 = null;
 		BufferedImage museum5 = null;
 		BufferedImage phonecall = null;
-		BufferedImage security = null;
 		BufferedImage security2 = null;
-		BufferedImage security3 = null;
-		BufferedImage security4 = null;
 		BufferedImage security5 = null;
-		BufferedImage security6 = null;
-		BufferedImage security7 = null;
 		Clip attack = null;
 		Clip doorknob = null;
 		Clip breathing = null;
@@ -63,14 +56,63 @@ public class AdventureGame {
 				System.err.println("There was an error loading the image.");
 				e.printStackTrace();
 			}
-			
+
 			try{
 				keyboard = AudioSystem.getClip();
-				keyboard.open(AudioSystem.getAudioInputStream(new File("keyboard.wav")));
+				keyboard.open(AudioSystem.getAudioInputStream(new File("AdentureGameAudio/keyboard.wav")));
 
 			}catch  (Exception e) {
 				e.printStackTrace();
 			}
+
+			try{
+				attack = AudioSystem.getClip();
+				attack.open(AudioSystem.getAudioInputStream(new File("AdentureGameAudio/attack.wav")));
+
+			}catch  (Exception e) {
+				e.printStackTrace();
+			}
+
+			try{
+				doorknob = AudioSystem.getClip();
+				doorknob.open(AudioSystem.getAudioInputStream(new File("AdentureGameAudio/doorknob.wav")));
+
+			}catch  (Exception e) {
+				e.printStackTrace();
+			}
+
+			try{
+				breathing = AudioSystem.getClip();
+				breathing.open(AudioSystem.getAudioInputStream(new File("AdentureGameAudio/breathing.wav")));
+
+			}catch  (Exception e) {
+				e.printStackTrace();
+			}
+
+			try{
+				helicopter = AudioSystem.getClip();
+				helicopter.open(AudioSystem.getAudioInputStream(new File("AdentureGameAudio/helicopter.wav")));
+
+			}catch  (Exception e) {
+				e.printStackTrace();
+			}
+
+			try{
+				siren = AudioSystem.getClip();
+				siren.open(AudioSystem.getAudioInputStream(new File("AdentureGameAudio/siren.wav")));
+
+			}catch  (Exception e) {
+				e.printStackTrace();
+			}
+
+			try{
+				punches = AudioSystem.getClip();
+				punches.open(AudioSystem.getAudioInputStream(new File("AdentureGameAudio/punches.wav")));
+
+			}catch  (Exception e) {
+				e.printStackTrace();
+			}
+
 			c.drawImage(outside, 0, 0, null);
 
 
@@ -84,13 +126,13 @@ public class AdventureGame {
 			c.println("Ruby Necklace");
 			c.println(" ");
 
-			///array to hold these items
+			//array to hold these items
 			String []items = new String[3];
 			for (int i = 0; i < items.length; i++)
 			{
 				items[i] = c.readLine().toLowerCase(); // saves the items that they stole into an array
 				while(!(items[i].equals("bedazzled gun") || items[i].equals("royal crown") || items[i].equals("golden dagger") || items[i].equals("ancient vase") || items[i].equals("ruby necklace"))){
-					c.println("Invalid answer please answer again");
+					c.println("Invalid answer please answer again"); //while loop for spell-check
 					items[i] = c.readLine().toLowerCase();
 
 				}
@@ -104,8 +146,8 @@ public class AdventureGame {
 			}
 			c.drawImage(phonecall, 0, 0, null);
 
-			c.println("Your friend calls to tell you that his car broke down and he can't pick you up \nanymore. \nHe says his friend can pick you up in his helicopter but you have to wait 30 mins. \nDo you want to wait for the helicopter or make a run for it? (run/wait)");									
-			String run = c.readLine().toLowerCase(); // left side of flow chart
+			c.println("Your friend calls to tell you that his car broke down and he can't pick you up anymore. \nHe says his friend can pick you up in his helicopter but you have to wait 30 mins. \nDo you want to wait for the helicopter or make a run for it? (run/wait)");									
+			String run = c.readLine().toLowerCase(); // left side of flowchart
 			c.clear();
 			while(!(run.equals("run") || run.equals("wait"))){
 				c.println("Invalid answer please answer again");
@@ -120,7 +162,7 @@ public class AdventureGame {
 					System.err.println("There was an error loading the image.");
 					e.printStackTrace();
 				}
-				c.drawImage(lasersecurity, 0, 0, null);
+				c.drawImage(lasersecurity, 40, 150,40,150, null);
 
 				c.println("You begin to run but you see lasers ahead. Do you try to run through them or deactivate it?(pass/deactivate)");
 				String pass = c.readLine().toLowerCase();
@@ -176,15 +218,21 @@ public class AdventureGame {
 									e.printStackTrace();
 								}
 								c.drawImage(handcuffs, 0, 0, null);
+								siren.loop(Clip.LOOP_CONTINUOUSLY);
+								Thread.sleep(2000);
+								siren.stop();
+								
 								c.println("You run into the police and get caught. You failed the mission."); //end of mission
 								c.println("Although unsuccessful, you almost stole:");
 								for (int i = 0; i< items.length;i++)
 								{
 									c.println(items[i]);
 								}
+								
 
 							}
 							else {
+								c.drawImage(outside, 0, 0, null);
 								c.println("You manage to escape the museum unharmed. Mission accomplished! You successfully stole:"); //end of mission
 								for (int i = 0; i< items.length;i++)
 								{
@@ -231,14 +279,18 @@ public class AdventureGame {
 						System.err.println("There was an error loading the image.");
 						e.printStackTrace();
 					}
-					c.drawImage(keypad, 0, 0, null);
+					c.drawImage(keypad, 25, 25, null);
 					c.println("There is a keypad on the wall to deactivate the lasers. This is protected with a riddle. The riddle is: What goes up, and doesn't go back down? Hint: the answer is a three letter word.");
 					int counter = 0;
 					while ( counter <3)
 					{
 						String answer = c.readLine().toLowerCase();
-						c.clear();
+						keyboard.loop(Clip.LOOP_CONTINUOUSLY);
+						Thread.sleep(2000);
+						keyboard.stop();
 						if (answer.equals("age")) {
+							c.clear();
+							c.drawImage(outside, 0, 0, null);
 							c.println("The lasers have deactivated and you safely escape the museum. Mission accomplished!"); //end of mission
 							for (int i = 0; i< items.length;i++)
 							{
@@ -257,6 +309,13 @@ public class AdventureGame {
 
 			}
 			else {
+				try {
+					museum2 = ImageIO.read(new File("AdventureGame/museum2.jpg"));
+				} catch (IOException e) {
+					System.err.println("There was an error loading the image.");
+					e.printStackTrace();
+				}
+				c.drawImage(museum2, 0, 0, null);
 				//right side of flowchart (helicopter)
 				c.println("Do you want to wait upstairs or downstairs? (up/down)");
 				String up = c.readLine().toLowerCase();
@@ -267,7 +326,14 @@ public class AdventureGame {
 					c.clear();
 				}
 				if (up.equals("up")) {
-					c.println("Your phone begins to ring but it's an unknown number. Do you pick up? (yes/no)"); //ringtone sound
+					try {
+						phonecall = ImageIO.read(new File("AdventureGame/phonecall.jpg"));
+					} catch (IOException e) {
+						System.err.println("There was an error loading the image.");
+						e.printStackTrace();
+					}
+					c.drawImage(phonecall, 0, 0, null);
+					c.println("Your phone begins to ring but it's an unknown number. Do you pick up? (yes/no)");
 					String phone = c.readLine().toLowerCase();
 					c.clear();
 					while(!(phone.equals("yes") || phone.equals("no"))){
@@ -276,25 +342,51 @@ public class AdventureGame {
 						c.clear();
 					}
 					if (phone.equals("yes")) {
-						c.println("The cops were calling you and you got busted! You failed the mission."); //police siren sound, end of mission
+						siren.loop(Clip.LOOP_CONTINUOUSLY);
+						try {
+							handcuffs = ImageIO.read(new File("AdventureGame/handcuffs.jpg"));
+						} catch (IOException e) {
+							System.err.println("There was an error loading the image.");
+							e.printStackTrace();
+						}
+						c.drawImage(handcuffs, 0, 0, null);
+						c.println("The cops were calling you and you got busted! You failed the mission."); //end of mission
 						c.println("Although unsuccessful, you almost stole:");
 						for (int i = 0; i< items.length;i++)
 						{
 							c.println(items[i]);
 						}
+						Thread.sleep(3000);
+						siren.stop();
 					}
 					else {
-						c.println("The helicopter guy called you. Mission accomplished! You successfully stole:"); //end of mission
+						helicopter.loop(Clip.LOOP_CONTINUOUSLY);
+						try {
+							outside = ImageIO.read(new File("AdventureGame/outsidemuesuml.jpg"));
+						} catch (IOException e) {
+							System.err.println("There was an error loading the image.");
+							e.printStackTrace();
+						}
+						c.drawImage(outside, 0, 0, null);
+						c.println("The helicopter guy called you and you escape the museum. Mission accomplished! You successfully stole:"); //end of mission
 						for (int i = 0; i< items.length;i++)
 						{
 							c.println(items[i]);
 						}
+						Thread.sleep(3000);
+						helicopter.stop();
 					}
 				}
 				else {
-
+					try {
+						keypad = ImageIO.read(new File("AdventureGame/keypad.jpg"));
+					} catch (IOException e) {
+						System.err.println("There was an error loading the image.");
+						e.printStackTrace();
+					}
+					c.drawImage(keypad, 0, 0, null);
 					keyboard.loop(Clip.LOOP_CONTINUOUSLY);
-					c.println("The basement is protected by a keypad. Can you guess the code? \n30/5+4*(-2)=");
+					c.println("You go downstairs to the basement but it is protected by a keypad. Can you guess the code? \n30/5+4*(-2)=");
 					double math;
 					try{
 						math = Double.parseDouble(c.readLine());
@@ -305,19 +397,58 @@ public class AdventureGame {
 					Thread.sleep(2000);
 					keyboard.stop();
 					int counter1 = 0;
-					while(counter1 < 3){
+					while(counter1 <= 3){
 
 
 						if (math == 0) {
 							c.clear();
+							try {
+								basement1 = ImageIO.read(new File("AdventureGame/basement1.jpg"));
+							} catch (IOException e) {
+								System.err.println("There was an error loading the image.");
+								e.printStackTrace();
+							}
+							c.drawImage(basement1, 0, 0, null);
+
 							c.println("You get into the basement but now you have to wait 30 mins for the helicopter. \nThe time has passed and you get a text from the pilot. \nYou realize you have bad service since you're unable to text him back. Do you go upstairs to receive service or stay downstairs? (up/stay)");
 							String cell = c.readLine().toLowerCase();
 							if(cell.equals("stay")){
+								c.clear();
+								try {
+									security5 = ImageIO.read(new File("AdventureGame/security5.jpg"));
+								} catch (IOException e) {
+									System.err.println("There was an error loading the image.");
+									e.printStackTrace();
+								}
+								c.drawImage(security5, 0, 0, null);
 								c.println("A guard enters the basement. Do you hide or try to fight them? (hide/fight)");
 								String hide = c.readLine().toLowerCase();
 								if (hide.equals("hide")) {
+									try {
+										basement2 = ImageIO.read(new File("AdventureGame/basement2.jpg"));
+									} catch (IOException e) {
+										System.err.println("There was an error loading the image.");
+										e.printStackTrace();
+									}
+									c.drawImage(basement2, 0, 0, null);
+									doorknob.loop(Clip.LOOP_CONTINUOUSLY);
 									c.println("You enter a room but you can't open it from the inside. You get locked inside until a guard finds you. Mission unsuccessful."); //end of mission
-
+									Thread.sleep(2000);
+									doorknob.stop();
+								} 
+								else{
+									try {
+										handcuffs = ImageIO.read(new File("AdventureGame/handcuffs.jpg"));
+									} catch (IOException e) {
+										System.err.println("There was an error loading the image.");
+										e.printStackTrace();
+									}
+									c.drawImage(handcuffs, 0, 0, null);
+									c.println("You lose the fight again the guard. You almost stole:");
+									for (int i = 0; i< items.length;i++)
+									{
+										c.println(items[i]);
+									}
 								}
 
 
@@ -325,21 +456,39 @@ public class AdventureGame {
 
 							else {//going upstairs for cell service
 								double chance = Math.random(); // random number generator 50% chance of getting caught
-								if (chance < 0.5){ 
+								if (chance < 0.5){
+									helicopter.loop(Clip.LOOP_CONTINUOUSLY);
+									try {
+										outside = ImageIO.read(new File("AdventureGame/outsidemuseum.jpg"));
+									} catch (IOException e) {
+										System.err.println("There was an error loading the image.");
+										e.printStackTrace();
+									}
+									c.drawImage(outside, 0, 0, null);
 									c.println("You manage to escape the museum safely on the helicopter. Mission accomplished! You successfully stole:"); //end of mission
 									for (int i = 0; i< items.length;i++)
 									{
 										c.println(items[i]);
 									}
+									Thread.sleep(3000);
+									helicopter.stop();
 								}
 								else {
+									try {
+										museum5 = ImageIO.read(new File("AdventureGame/museum5.jpg"));
+									} catch (IOException e) {
+										System.err.println("There was an error loading the image.");
+										e.printStackTrace();
+									}
+									c.drawImage(museum5, 0, 0, null);
 									c.println("You see guards and decide to fight them off.");
 									boolean found = false;
 									for (int i = 0; i < items.length;i++)
 									{
-										if (items[i].equals("golden dagger") || items[i].equals("bedazzled gun")){
-											c.println("You manage to win the fight with " + items[i]);
-											c.println("You succesfully escape the museum.");
+										if (items[i].equals("golden dagger") || items[i].equals("bedazzled gun")){ 
+											c.println("You manage to win the fight with " + items[i]); 
+											c.drawImage(outside, 0, 0, null);
+											c.println("You successfully escape the museum! You managed to steal:"); //end of mission
 											c.println(items[0]);
 											c.println(items[1]);
 											c.println(items[2]);
@@ -348,11 +497,21 @@ public class AdventureGame {
 										}
 									}
 									if(!found){
-										c.println("You failed the mission. You almost stole:");
+										punches.loop(Clip.LOOP_CONTINUOUSLY);
+										try {
+											security2 = ImageIO.read(new File("AdventureGame/security2.jpg"));
+										} catch (IOException e) {
+											System.err.println("There was an error loading the image.");
+											e.printStackTrace();
+										}
+										c.drawImage(security2, 0, 0, null);
+										c.println("You try to fight the guards with your fists due to the lack of weapons you have and fail. You almost stole:");
 										for (int i = 0; i< items.length;i++)
 										{
 											c.println(items[i]);
 										}
+										Thread.sleep(4000);
+										punches.stop();
 									}
 
 
@@ -362,11 +521,23 @@ public class AdventureGame {
 						else{
 							c.println("That was incorrect, try again."); 
 							counter1 = counter1 +1;
-							math = c.readDouble();
+							try{
+								math = Double.parseDouble(c.readLine());
+							}catch (Exception e){
+								c.println("Please enter a number digit");
+								math = -1;
+							}
 						}
-
+											
 					}
-					c.println("You're out of guesses and set off the alarms. You failed the mission. You almost stole:");
+					try {
+						handcuffs = ImageIO.read(new File("AdventureGame/handcuffs.jpg"));
+					} catch (IOException e) {
+						System.err.println("There was an error loading the image.");
+						e.printStackTrace();
+					}
+					c.drawImage(handcuffs, 0, 0, null);
+					c.println("You're out of guesses and set off the alarms. You failed the mission. You almost stole:"); //end of mission
 					for (int i = 0; i< items.length;i++)
 					{
 						c.println(items[i]);
@@ -376,15 +547,15 @@ public class AdventureGame {
 
 
 
-			c.print("Do you want to play again?(y/n)");// how to make all the endings play this message?
+			c.print("Do you want to play again?(y/n)"); //play again or restart the game
 			playagain = c.readLine().toLowerCase(); 
 			while(!(playagain.equals("y") || playagain.equals("n"))){
 				c.println("Invalid answer please answer again");
 				playagain = c.readLine().toLowerCase();
 				c.clear();
 			}
-
-		}while (playagain.equals("y"));
+c.clear();
+}while (playagain.equals("y"));
 		c.close();
 
 	} 
@@ -394,9 +565,4 @@ public class AdventureGame {
 
 
 }
-//to do list
-//photos and sounds
-// fancy typing
-
-
 
