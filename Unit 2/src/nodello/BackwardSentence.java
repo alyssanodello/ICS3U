e21f7a29a4888676d9c1e7c988301d55096e2984
323966051 <323966051@C-DS24WR1.tdsb.on.ca>
2017-04-13 14:18:06 -0400

    Capital letters work but cant do periods

---
+++ Unit 2/src/nodello/BackwardSentence.java
@@ -0,0 +1,26 @@
+package nodello;
+
+import java.util.Scanner;
+
+public class BackwardSentence {
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+	System.out.println("Enter a sentence");
+	String[] myString = sc.nextLine().split(" ");
+	for(int i = 0; i < myString.length; i ++){
+		for(int a = myString[i].length()-1; a>=0; a--){
+			if(a== myString[i].length()-1 &&i==0){
+				System.out.print(Character.toUpperCase(myString[i].charAt(a)));
+			}
+			
+			else{
+				System.out.print(Character.toLowerCase(myString[i].charAt(a)));
+			}
+		}
+		System.out.print(" ");
+	}
+		
+
+	}
+
+}
