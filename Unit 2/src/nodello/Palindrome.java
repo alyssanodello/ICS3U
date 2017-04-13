14db29261352674fc727bea159ed65c8af4a4be6
323966051 <323966051@C-DS24WR1.tdsb.on.ca>
2017-04-13 14:58:24 -0400

    updated version that now works for even number words

--- Unit 2/src/nodello/Palindrome.java
+++ Unit 2/src/nodello/Palindrome.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Palindrome {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a word to see if it is a palindrome");
 		String word = sc.nextLine();
@@ -15,24 +15,36 @@ public class Palindrome {
 		else{
 			System.out.println("That word is not a palindrome");
 		}
-		
+
 	}
 	public static boolean isPalindrome(String word){
 		boolean palindrome = false;
-		for(int a= 0; a<word.length()/2; a++){
-			for (int b = word.length()-1; b> word.length()/2; b--){
-				if( word.charAt(a)== word.charAt(b)){
-					palindrome = true;
-				}
-				else{
-					palindrome = false;
+		if(word.length()%2 == 1){
+			for(int a= 0; a<word.length()/2; a++){
+				for (int b = word.length()-1; b> word.length()/2; b--){
+					if( word.charAt(a)== word.charAt(b)){
+						palindrome = true;
+					}
+					else{
+						palindrome = false;
+					}
 				}
 			}
 		}
-		return palindrome;
-		
-		
-		
-	}
+		else {
+			for(int a= 0; a<word.length()-1; a++){
+				for (int b = word.length()-1; b> 0; b--){
+					if( word.charAt(a)== word.charAt(b)){
+						palindrome = true;
+					}
+					else{
+						palindrome = false;
+					}
+				}
+			}
+		}
+	return palindrome;
 
 }
+}
+
